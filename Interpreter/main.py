aeb47498ccec3536832a648b86115743c8f0f8f4
MarlonCosta <marlon.costa@ieee.org>
2018-12-02 03:25:11 -0300

    Adicionando o projeto

---
+++ Interpreter/main.py
@@ -0,0 +1,269 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = (
+    'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF'
+)
+
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return "{}".format(self.value)
+
+
+class Lexer(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Caractere inválido')
+
+    def advance(self):
+        """Avança a posição do ponteiro"""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        """Pula espaços"""
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        """Captura inteiros com vários dígitos"""
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        """Analisador léxico"""
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MUL, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/')
+
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
+
+            self.error()
+
+        return Token(EOF, None)
+
+
+class AST(object):
+    pass
+
+
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+    def __str__(self):
+        return self.token.__str__()
+
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+    def __str__(self):
+        return self.token.__str__()
+
+
+class Parser(object):
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Sintaxe inválida')
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def factor(self):
+        """factor : INTEGER | LPAREN expr RPAREN"""
+        token = self.current_token
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            return Num(token)
+        elif token.type == LPAREN:
+            self.eat(LPAREN)
+            node = self.expr()
+            self.eat(RPAREN)
+            return node
+
+    def term(self):
+        """term : factor ((MUL | DIV) factor)*"""
+        node = self.factor()
+
+        while self.current_token.type in (MUL, DIV):
+            token = self.current_token
+            if token.type == MUL:
+                self.eat(MUL)
+            elif token.type == DIV:
+                self.eat(DIV)
+
+            node = BinOp(left=node, op=token, right=self.factor())
+
+        return node
+
+    def expr(self):
+        """
+        expr   : term ((PLUS | MINUS) term)*
+        term   : factor ((MUL | DIV) factor)*
+        factor : INTEGER | LPAREN expr RPAREN
+        """
+        node = self.term()
+
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+            elif token.type == MINUS:
+                self.eat(MINUS)
+
+            node = BinOp(left=node, op=token, right=self.term())
+
+        return node
+
+    def parse(self):
+        node = self.expr()
+        if self.current_token.type != EOF:
+            self.error()
+        return node
+
+
+class node_visitor(object):
+    def visit(self, node, parent=None):
+        method_name = 'visit_' + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)
+        if parent:
+            return visitor(node, parent)
+        else:
+            return visitor(node)
+
+    def generic_visit(self, node):
+        raise Exception('Método visit_{} não existe'.format(type(node).__name__))
+
+
+class Interpreter(node_visitor):
+    def __init__(self, parser):
+        self.parser = parser
+        self.tree = nx.Graph()
+        self.node_count = 0
+
+    def visit_BinOp(self, node, parent=None):
+        self.node_count += 1
+        if parent:
+            self.tree.add_node(parent, label=parent.token.__str__())
+            self.tree.add_node(node, label=node.token.__str__())
+            self.tree.add_edge(parent, node)
+
+        else:
+            self.tree.add_node(node, label=node.token)
+
+        if node.op.type == PLUS:
+            return self.visit(node.left, node) + self.visit(node.right, node)
+        elif node.op.type == MINUS:
+            return self.visit(node.left, node) - self.visit(node.right, node)
+        elif node.op.type == MUL:
+            return self.visit(node.left, node) * self.visit(node.right, node)
+        elif node.op.type == DIV:
+            return self.visit(node.left, node) / self.visit(node.right, node)
+
+    def visit_Num(self, node, parent=None):
+        if parent:
+            self.tree.add_node(parent, label=parent.token.__str__())
+            self.tree.add_node(node, label=node.token.__str__())
+            self.tree.add_edge(parent, node)
+        return node.value
+
+    def interpret(self):
+        tree = self.parser.parse()
+        return self.visit(tree)
+
+
+def hierarchy_pos(G, root, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5,
+                  pos=None, parent=None):
+    if not pos:
+        pos = {root: (xcenter, vert_loc)}
+    else:
+        pos[root] = (xcenter, vert_loc)
+    neighbors = list(G.neighbors(root))
+    if parent:  # this should be removed for directed graphs.
+        neighbors.remove(parent)  # if directed, then parent not in neighbors.
+    if len(neighbors) != 0:
+        dx = width / len(neighbors)
+        nextx = xcenter - width / 2 - dx / 2
+        for neighbor in neighbors:
+            nextx += dx
+            pos = hierarchy_pos(G, neighbor, width=dx, vert_gap=vert_gap,
+                                vert_loc=vert_loc - vert_gap, xcenter=nextx, pos=pos,
+                                parent=root)
+    return pos
+
+
+def main():
+    while True:
+        try:
+            text = input('calc> ')
+        except EOFError:
+            break
+        if not text:
+            continue
+
+        lexer = Lexer(text)
+        parser = Parser(lexer)
+        interpreter = Interpreter(parser)
+        result = interpreter.interpret()
+        print("= {}".format(result))
+        pos = hierarchy_pos(interpreter.tree, list(interpreter.tree)[0])
+        nx.draw_networkx(interpreter.tree, pos, arrows=False, node_size=1500.0, font_color='white', font_weight="bold",
+                         font_size=25.0)
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
